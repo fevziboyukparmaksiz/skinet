aefafe6f4213e79f7474e48b8bc03a8ea365dae2
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-31 22:54:31 +0300

    Sorting

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -15,10 +15,21 @@ namespace Core.Specifications
 
         public Expression<Func<T, bool>> Criteria { get; }
         public List<Expression<Func<T, object>>> Includes { get; } = new();
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
 
         protected void AddInclude(Expression<Func<T, object>> includeExExpression)
         {
             Includes.Add(includeExExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
         }
     }
 }
