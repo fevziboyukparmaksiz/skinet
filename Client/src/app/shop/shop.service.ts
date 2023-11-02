cfcada4dee5e11e12a3c595a363059c487270a9c
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-11-02 22:55:36 +0300

    Designing the shop page

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl: string = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Pagination<Product[]>> {
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
 }
