7b9767c9d1fbd9493650434dc3f595e3cb7284c1
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-11-06 23:08:38 +0300

    pagination finished

--- Client/src/app/shop/shop.module.ts
+++ Client/src/app/shop/shop.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,7 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
   ],
   imports: [
     CommonModule,
-    PaginationModule
+    SharedModule
   ],
   exports: [
     ShopComponent
