c27797f699efac940d1d73f3609bac52dba6748d
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-11-15 23:52:53 +0300

    Basket - Client

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
+  constructor(private basketService: BasketService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) this.basketService.getBasket(basketId);
   }
 
 }
